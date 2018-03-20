da7f8843aec8a2d0dd014a847963a501466d3775
Marciano Preciado <marcianocpreciado@gmail.com>
2018-03-19 21:56:30 -0600

    Add Measurement Functionality to Experimental Setup

--- Experiments/Arduino-Interface/Arduino-Interface.ino
+++ Experiments/Arduino-Interface/Arduino-Interface.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+
 // Definition of message bytes
 const byte START_BYTE = 1;
 const byte STOP_BYTE = 27;
@@ -16,21 +18,22 @@ enum Probe {PH, EC};
 
 // Function Prototypes
 void write_input(Input input, bool state);
-uint16_t get_measurement(Probe probe);
+float get_measurement(Probe probe);
 uint16_t get_duration();
 Input get_input();
-void send_data(uint16_t t, uint16_t data, bool input);
-
-
-
+void send_data(uint16_t t, float data, bool input);
+float read_ph();
+float read_ec();
 
 // Global Variables
-int f = 20;                     // Sampling frequency
 uint16_t pre_duration = 2000;   // Duration of data sampling before input
 uint16_t post_duration = 10000; // Duration of data sampling after input
+const int ph_addr = 0x63;
+const int ec_addr = 0x64;
 
 void setup() {
   Serial.begin(9600);
+  Wire.begin();
 }
 
 void loop() {
@@ -55,43 +58,33 @@ void loop() {
 
       // Send measurements before input is activated
       while (millis() - t0 <= pre_duration) {
-        // Send samples at f Hz
-        if (millis() - t_last > 1000 / f) {
-          measurement = get_measurement(probe);
-          t_last = millis();
-          t = t_last - t0;
-          send_data(t, measurement, 0);
-        }
+        measurement = get_measurement(probe);
+        t_last = millis();
+        t = t_last - t0;
+        send_data(t, measurement, 0);
       }
       // Send measurements while input is active
       write_input(input_type, true);
       uint16_t t1 = millis();
       while (millis() - t1 <= duration) {
-        // Send samples at f Hz
-        if (millis() - t_last > 1000 / f) {
-          measurement = get_measurement(probe);
-          t_last = millis();
-          t = t_last - t0;
-          send_data(t, measurement, 1);
-        }
+        measurement = get_measurement(probe);
+        t_last = millis();
+        t = t_last - t0;
+        send_data(t, measurement, 1);
       }
       // Send measurements after input is deactivated
       write_input(input_type, false);
       uint16_t t2 = millis();
       while (millis() - t2 <= post_duration) {
-        // Send samples at f Hz
-        if (millis() - t_last > 1000 / f) {
-          measurement = get_measurement(probe);
-          t_last = millis();
-          t = t_last - t0;
-          send_data(t, measurement, 0);
-        }
+        measurement = get_measurement(probe);
+        t_last = millis();
+        t = t_last - t0;
+        send_data(t, measurement, 0);
       }
       Serial.write(STOP_BYTE);
     }
   }
 }
-
 
 
 /* -------- FUNCTION DEFINITIONS --------- */
@@ -103,13 +96,13 @@ void write_input(Input input, bool state) {
 
 /* Gets measurement from the requested probe
     TODO: Implement measurement collection */
-uint16_t get_measurement(Probe probe) {
-  uint16_t measurement = 0;
+float get_measurement(Probe probe) {
+  float measurement = 0;
   if (probe == PH) {
-    measurement = 200;
+    measurement = read_ph();
   }
   else if (probe == EC) {
-    measurement = 100;
+    measurement = read_ec();
   }
   return measurement;
 }
@@ -148,7 +141,14 @@ Input get_input() {
 
 /* Sends an idenification byte as to whether the input is on, then sends
     the data in two bytes, LSB first */
-void send_data(uint16_t t, uint16_t data, bool input) {
+void send_data(uint16_t t, float data, bool input) {
+  byte lsb, msb, b[4];
+  union Swap {
+    float data;
+    uint32_t raw;
+  };
+
+  unsigned char ascii[32];
   // Send input ID
   if (input)
     Serial.write(INPUT_ID_BYTE);
@@ -156,15 +156,118 @@ void send_data(uint16_t t, uint16_t data, bool input) {
     Serial.write(NO_INPUT_ID_BYTE);
 
   // Send time stamp
-  byte lsb = 0xFF & t;
-  byte msb = t >> 8;
+  lsb = 0xFF & t;
+  msb = t >> 8;
   Serial.write(lsb);
   Serial.write(msb);
 
-  // Send data
-  lsb = data & 0xFF;
-  msb = data >> 8;
-  Serial.write(lsb);
-  Serial.write(msb);
+  // Convert float to unsigned 32 int so we can send it byte by byte
+  Swap swap = {data};
+  for (int i = 0; i < 4; i++)
+    Serial.write((swap.raw >> i * 8) & 0xFF);
+}
+
+/* Requests and returns a single pH reading from the EZO circuit
+*/
+float read_ph() {
+  char ph_data[20];
+  char next;
+  float ph = 0.0;
+
+  // Request reading
+  Wire.beginTransmission(ph_addr);
+  Wire.write('R');
+  Wire.endTransmission();
+  delay(950);
+  // Get Reading
+  Wire.requestFrom(ph_addr, 20, true);
+  byte response = Wire.read();
+  int i = 0;
+  switch (response) {
+    case 1: // Data ready
+      while (Wire.available()) {
+        next = Wire.read();
+        if (next == 0) {
+          Wire.endTransmission();
+          break;
+        }
+        ph_data[i++] = next;
+      }
+      ph = atof(ph_data);
+      break;
+    case 2: // Failed reading
+      ph = -1;
+      break;
+    case 254: // Data pending
+      delay(100);
+      Wire.requestFrom(ph_addr, 20, true);
+      response = Wire.read();
+      while (Wire.available()) {
+        next = Wire.read();
+        if (next == 0) {
+          Wire.endTransmission();
+          break;
+        }
+        ph_data[i++] = next;
+      }
+      ph = atof(ph_data);
+      break;
+    case 255: // No data
+      ph = -1;
+      break;
+  };
+  return ph;
+}
+
+/* Requests and returns a single EC reading from the EZO circuit
+*/
+float read_ec() {
+  char ec_data[20];
+  char next;
+  float ec = 0.0;
+
+  // Request reading
+  Wire.beginTransmission(ec_addr);
+  Wire.write('R');
+  Wire.endTransmission();
+  delay(600);
+  // Get Reading
+  Wire.requestFrom(ec_addr, 20, true);
+  byte response = Wire.read();
+  int i = 0;
+  switch (response) {
+    case 1: // Data ready
+      while (Wire.available()) {
+        next = Wire.read();
+        if (next == 0) {
+          Wire.endTransmission();
+          break;
+        }
+        ec_data[i++] = next;
+      }
+      ec = atof(ec_data);
+      break;
+    case 2: // Failed reading/syntax error
+      ec = -1;
+      break;
+    case 254: // Data pending
+      delay(100);
+      Wire.requestFrom(ec_addr, 20, true);
+      response = Wire.read();
+      while (Wire.available()) {
+        next = Wire.read();
+        if (next == 0) {
+          Wire.endTransmission();
+          break;
+        }
+        ec_data[i++] = next;
+      }
+      ec = atof(ec_data);
+      break;
+    case 255: // No data
+      ec = -1;
+      break;
+  };
+  return ec;
 }
 
